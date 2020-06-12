fc39c30d1ad57d53def74dcab31f629b1edb3a3b
Xin Wang <xin.victorwang@gmail.com>
2020-06-12 18:41:56 +0800

    remove useless code

--- elastic-job-spring-boot-samples/elastic-job-spring-boot-samples-jobs/src/main/java/io/wangxin/elasticjob/example/jobs/ExampleJobsApplication.java
+++ elastic-job-spring-boot-samples/elastic-job-spring-boot-samples-jobs/src/main/java/io/wangxin/elasticjob/example/jobs/ExampleJobsApplication.java
@@ -2,13 +2,11 @@ package io.wangxin.elasticjob.example.jobs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 /**
  * @Author Wang Xin
  */
 @SpringBootApplication(scanBasePackages = "io.wangxin.elasticjob")
-//@ImportResource("classpath:META-INF/applicationContext.xml")
 public class ExampleJobsApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExampleJobsApplication.class, args);
